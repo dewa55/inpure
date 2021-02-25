b99b8d56babda96fd0d2a683fb97808c3408a3f4
dewa55 <vedran.devcic55@gmail.com>
2021-02-25 11:09:24 +0100

    Update docker-entrypoint.sh

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -3,8 +3,6 @@
 # Set up .htpasswd
 cd /etc/apache2/
 echo 'infinum:$apr1$u.laun1w$x6Xi6QmYlpPPR36XTRnmh/' > .htpasswd
-
 rm -f /run/apache2/apache2.pid
 rm -f /var/run/apache2/apache2.pid
-
 exec apache2 -DFOREGROUND "$@"
