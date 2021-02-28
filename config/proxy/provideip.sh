b22618d5980aa33227d01dec565282759426255b
Vedran <dewa@VDEVCIC-T580.intranet.combis.hr>
2021-02-28 05:38:52 +0100

    proxy settings script edited

--- config/proxy/provideip.sh
+++ config/proxy/provideip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 az container list | jq -r '.[].ipAddress.ip' >> ipaddressnginx.txt
-curl https://raw.githubusercontent.com/dewa55/inpure/main/config/proxyconf.conf > default.conf
-curl 
+curl https://raw.githubusercontent.com/dewa55/inpure/main/config/proxy/proxyconf.conf > default.conf
+curl https://raw.githubusercontent.com/dewa55/inpure/main/config/proxy/Dockerfile > Dockerfile
 myvar="$(cat ipaddressnginx.txt)"
 sed -i "s/changeme/$myvar/g" default.conf
 $DOCKER_PASS="mypass"
